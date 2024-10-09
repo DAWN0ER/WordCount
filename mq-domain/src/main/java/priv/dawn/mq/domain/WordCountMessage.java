ccd8b5aac56b1c631c47dfea46f55f94f590aaac
Dawn Yang <dawn112358@outlook.com>
2024-10-09 11:31:05 +0800

    重构结构 Mybatis 插件有 bug

---
+++ mq-domain/src/main/java/priv/dawn/mq/domain/WordCountMessage.java
@@ -0,0 +1,12 @@
+package priv.dawn.mq.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Description:
+ * @Auther: Dawn Yang
+ * @Since: 2024/10/09/11:16
+ */
+
+public class WordCountMessage {
+}
---
+++ word-count-api/src/main/java/priv/dawn/wordcount/api/Worker.java
@@ -0,0 +1,14 @@
+package priv.dawn.wordcount.api;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Description:
+ * @Auther: Dawn Yang
+ * @Since: 2024/10/09/11:05
+ */
+public interface Worker {
+
+
+
+}
