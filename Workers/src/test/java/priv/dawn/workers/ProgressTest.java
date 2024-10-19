b088ac9504ae7c3f07324f545a6941184dc8ddac
Dawn Yang <dawn112358@outlook.com>
2024-10-19 20:40:27 +0800

    重构项目: fix Zookeeper

--- Workers/src/test/java/priv/dawn/workers/ProgressTest.java
+++ Workers/src/test/java/priv/dawn/workers/ProgressTest.java
@@ -1,60 +1,50 @@
 package priv.dawn.workers;
 
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import priv.dawn.kafkamessage.message.CustomMessage;
-import priv.dawn.workers.mapper.ProgressMapper;
-import priv.dawn.workers.domain.ProgressDao;
-
-import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-public class ProgressTest extends WorkersApplicationTests {
-
-    @Autowired
-    private ProgressMapper mapper;
-
-    @Autowired
-    private ProgressDao manager;
-
-    @Test
-    public void mapperTest() {
-        int uid = 996;
-        Float f = mapper.getProgress(uid);
-        log.info("if progress don't exist, the getProgress is: " + f);
-        manager.createProgress(uid, 3);
-        mapper.progressAdvanceOne(uid);
-        f = mapper.getProgress(uid);
-        log.info("if progress is " + uid + " the getProgress is: " + f);
-    }
-
-    @Test
-    public void mapperTestForInc() {
-        int uid = 999;
-        mapper.progressAdvanceOne(uid);
-        Float f = mapper.getProgress(uid);
-        log.info("if progress is " + uid + " the getProgress is: " + f);
-    }
-
-    @Test
-    public void threadTest() {
-        int pn = 4;
-        int uid = 996;
-        String json = new CustomMessage(new ArrayList<>()).toJsonStr();
-        ProducerRecord<String, String> record = new ProducerRecord<>("topic", 0, String.valueOf(uid), json);
-        ExecutorService pool = Executors.newFixedThreadPool(pn);
-        int loop = pn;
-        while (loop-- > 0) {
-            pool.execute(() -> {
-                log.info(Thread.currentThread().getName() + " working");
-                manager.updateProgress(record, pn);
-            });
-        }
-
-
-    }
+@Deprecated
+public class ProgressTest{
+//
+//    @Autowired
+//    private ProgressMapper mapper;
+//
+//    @Autowired
+//    private ProgressDao manager;
+//
+//    @Test
+//    public void mapperTest() {
+//        int uid = 996;
+//        Float f = mapper.getProgress(uid);
+//        log.info("if progress don't exist, the getProgress is: " + f);
+//        manager.createProgress(uid, 3);
+//        mapper.progressAdvanceOne(uid);
+//        f = mapper.getProgress(uid);
+//        log.info("if progress is " + uid + " the getProgress is: " + f);
+//    }
+//
+//    @Test
+//    public void mapperTestForInc() {
+//        int uid = 999;
+//        mapper.progressAdvanceOne(uid);
+//        Float f = mapper.getProgress(uid);
+//        log.info("if progress is " + uid + " the getProgress is: " + f);
+//    }
+//
+//    @Test
+//    public void threadTest() {
+//        int pn = 4;
+//        int uid = 996;
+//        String json = new CustomMessage(new ArrayList<>()).toJsonStr();
+//        ProducerRecord<String, String> record = new ProducerRecord<>("topic", 0, String.valueOf(uid), json);
+//        ExecutorService pool = Executors.newFixedThreadPool(pn);
+//        int loop = pn;
+//        while (loop-- > 0) {
+//            pool.execute(() -> {
+//                log.info(Thread.currentThread().getName() + " working");
+//                manager.updateProgress(record, pn);
+//            });
+//        }
+//
+//
+//    }
 
 
 }
