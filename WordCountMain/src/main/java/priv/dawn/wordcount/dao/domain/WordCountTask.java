bf432014601535c0182c16f15e8042aeed4a4a14
Dawn Yang <dawn112358@outlook.com>
2024-10-27 17:02:48 +0800

    重构项目: 新建数据表

---
+++ WordCountMain/src/main/java/priv/dawn/wordcount/dao/domain/WordCountTask.java
@@ -0,0 +1,65 @@
+package priv.dawn.wordcount.dao.domain;
+
+import java.util.Date;
+
+public class WordCountTask {
+    private Long id;
+
+    private Long taskId;
+
+    private Integer fileUid;
+
+    private Integer status;
+
+    private Date createdTime;
+
+    private Date updatedTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public Integer getFileUid() {
+        return fileUid;
+    }
+
+    public void setFileUid(Integer fileUid) {
+        this.fileUid = fileUid;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public Date getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(Date updatedTime) {
+        this.updatedTime = updatedTime;
+    }
+}
